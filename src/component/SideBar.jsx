0cbb5f133fc7c243b4cfb503a4a6230bfc1acf14
shubham singh Rathore <138260347+NandSingh007@users.noreply.github.com>
2023-08-24 03:48:10 -0700

    Your commit message here

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -10,7 +10,7 @@ function SideBar() {
     const [error, setError] = useState(null);
 
     // UseEffect to fetch data from the API
-   
+
     useEffect(() => {
         fetchData();
     }, [bookings]);
@@ -42,45 +42,47 @@ function SideBar() {
     return (
         <div>
             {loading ? (
-                            <div style={{ textAlign: 'center', marginTop: '150px' }}>
-                                <Spinner animation="grow" variant="info" />
-                            </div>
+                <div style={{ textAlign: 'center', marginTop: '150px' }}>
+                    <Spinner animation="grow" variant="info" />
+                </div>
+            ) : (
+                <div>
+                    <h1 className='side_bar_heading'>Booking List</h1>
+                    <ul className='movie_detail'>
+                        {bookings.length === 0 ? (
+                            <p>No previous data available</p>
                         ) : (
-                            <div>
-                                <h1 className='side_bar_heading'>Booking List</h1>
-                                <ul className='movie_detail'>
-                                    {bookings.length === 0 ? (
-                                        <p>No previous data available</p>
-                                    ) : (
-                                        bookings.map((booking) => (
-                                            // Add a conditional check to ensure booking is valid before rendering
-                                            booking && booking._id ? (
-                                                <li key={booking._id}>
-                                                    {booking.movie && (
-                                                        <p><span>Movie</span> : {booking.movie}</p>
-                                                    )}
-                                                    {booking.slot && (
-                                                        <p><span>Slot</span> : {booking.slot}</p>
-                                                    )}
-                                                    {booking.seats && (
-                                                        <div>
-                                                            <p><span>Seats</span> :</p>
-                                                            <ul>
-                                                                {Object.entries(booking.seats).map(([seat, count]) => (
-                                                                    <li key={seat}>
-                                                                        {seat}: {count}
-                                                                    </li>
-                                                                ))}
-                                                            </ul>
-                                                        </div>
-                                                    )}
-                                                </li>
-                                            ) : <p className='previus_Data'>No Previous Record</p>
-                                        ))
-                                    )}
-                                </ul>
-                            </div>
+                            bookings.map((booking) => (
+                                // Add a conditional check to ensure booking is valid before rendering
+                                booking && booking._id ? (
+                                    <li key={booking._id}>
+                                        {booking.movie && (
+                                            <p><span>Movie</span> : {booking.movie}</p>
+                                        )}
+                                        {booking.slot && (
+                                            <p><span>Slot</span> : {booking.slot}</p>
+                                        )}
+                                        {booking.seats && (
+    <div>
+        <p><span>Seats</span> :</p>
+        <ul>
+            {Object.entries(booking.seats).filter(([seat, count]) => count > 0).map(([seat, count]) => (
+                <li key={seat}>
+                    {seat}: {count}
+                </li>
+            ))}
+        </ul>
+    </div>
+)}
+
+
+                                    </li>
+                                ) : <p className='previus_Data'>No Previous Record</p>
+                            ))
                         )}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 }
